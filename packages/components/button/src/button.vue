709f9aceee87a78a7a1405b97f8ef9bb90caedaf
LLLgoyour <lllgoyour@protonmail.com>
2021-11-10 20:13:46 -0500

    咕咕

---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,51 @@
+<template>
+    <button
+        :class="[
+            'L-button',
+            buttonType ? 'L-button--' + buttonType : '',
+            buttonSize ? 'L-button--' + buttonSize : '',
+            {
+                'is-disabled': buttonDisabled,
+                'is-loading': buttonLoading,
+                'is-plain': buttonPlain,
+                'is-round': buttonRound,
+                'is-circle': buttonCircle,
+            },
+            ]"
+        :disabled="buttonDisabled || buttonLoading"
+        :autofocu="autofocus"
+        :type="buttonType"
+        @click="handleClick"
+    >
+        <L-icon v-if="buttonLoading" class="is-loading"><buttonLoading /></L-icon>
+        <L-icon v-else-if="buttonIcon">
+            <component :is="buttonIcon" />
+        </L-icon>
+        <span
+            v=if=""
+
+</template>
+
+<script lang="ts">
+import { inject, Text, defineComponent, computed} from 'vue';
+
+export default defineComponent({
+    name: 'LButton',
+
+    components: {
+        LIcon,
+        ButtonLoading,
+    },
+
+    props: buttonProps,
+    emits: buttonEmits,
+    
+    setup(props, { emit, slots }) {
+        const LButton
+        Group = inject(LButtonGroupKey, undefined)
+        if (autoInsertSpace.value && defaultSlot?.length === 1) {
+            
+        }
+    }
+})
+</script>
